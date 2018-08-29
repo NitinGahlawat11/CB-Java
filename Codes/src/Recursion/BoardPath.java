282941146e20754d39d9de918d29fc468dc6a4d2
NitinGahlawat11 <37336987+NitinGahlawat11@users.noreply.github.com>
2018-08-29 10:25:26 +0530

    Add files via upload

---
+++ Codes/src/Recursion/BoardPath.java
@@ -0,0 +1,32 @@
+package nitin;
+
+import java.util.ArrayList;
+
+public class BoardPath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(boardpath(0,10).size());
+	}
+	public static ArrayList<String> boardpath(int curr,int end){
+		if(curr==end) {
+			ArrayList<String> br=new ArrayList<>();
+			br.add("");
+		return br;
+		}
+		if(curr>end) {
+			ArrayList<String> br=new ArrayList<>();
+			return br;
+		}
+		
+		ArrayList<String> mr=new ArrayList<>();
+		for(int dice=1;dice<=6;dice++) {
+			ArrayList<String> rr= boardpath(dice+curr,end);
+			for(String rrs:rr) {
+				mr.add(dice+rrs);
+			}
+		}
+	return mr;
+	}
+
+}
