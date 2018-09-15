9ce81d9f094ae86b887926a91bb64d5ae6651615
NitinGahlawat11 <webnitin640@gmail.com>
2018-09-15 16:51:29 +0530

    binary trees

---
+++ Codes/src/binarytrees/Call.java
@@ -0,0 +1,12 @@
+package binarytrees;
+
+public class Call {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Binarytrees tree = new Binarytrees();
+		tree.display();
+		System.out.println(tree.height());
+	}
+
+}
